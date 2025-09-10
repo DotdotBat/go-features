785a85da544a40404b50ee27984d7c9071c9ae36
dotdotbat <dotdotbat@gmail.com>
2025-09-10 11:13:07 +0300

    file creation and deletion test

--- features/file_manipulation_test.go
+++ features/file_manipulation_test.go
@@ -9,9 +9,25 @@ import (
 func TestCanDeleteFile(t *testing.T) {
 	testDir := t.TempDir()
 	filename := filepath.Join(testDir, "deletion_example")
-	if os.IsExist(filepath.ErrBadPattern) {
+	_, err := os.Stat(filename)
+	if !os.IsNotExist(err) {
 		t.Fatal("File exists in the temp path", filename, "Before being created")
 	}
-	os.WriteFile(filename, []byte("These are the test file contents"))
+	err = os.WriteFile(filename, []byte("These are the test file contents"), 0666)
+	if err != err {
+		panic(err)
+	}
+	_, err = os.Stat(filename)
+	if os.IsNotExist(err) {
+		t.Fatal("File not found after it was created.", filename)
+	}
+	err = os.Remove(filename)
+	if err != nil {
+		panic(err)
+	}
+	_, err = os.Stat(filename)
+	if !os.IsNotExist(err) {
+		t.Fatal("File still exists after it was deleted.", filename)
+	}
 
 }
